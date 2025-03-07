7b16dda9d0acf84bcf07c747de28d8d906d70ca9
Ivan Ivanchenko <ivanisovich@yandex.ru>
2025-03-08 00:30:34 +0700

    test

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="black-square"></div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.black-square {
+  width: 200px;
+  height: 200px;
+  background-color: black;
+}
+</style>
